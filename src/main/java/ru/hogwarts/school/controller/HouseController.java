b8eeda7c351218e95716d28870366d0093c7d105
Nikita Bolshakov <bnikmik@gmail.com>
2023-03-10 15:58:37 +0300

    done

--- src/main/java/ru/hogwarts/school/controller/HouseController.java
+++ src/main/java/ru/hogwarts/school/controller/HouseController.java
@@ -2,6 +2,8 @@ package ru.hogwarts.school.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.dto.FacultyDTO;
+import ru.hogwarts.school.dto.StudentDTO;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.service.HouseService;
 
@@ -17,14 +19,14 @@ public class HouseController {
     }
 
     @PostMapping("/faculty")
-    public ResponseEntity<Faculty> createFaculty(@RequestBody Faculty faculty) {
-        Faculty createdFaculty = houseService.createFaculty(faculty);
+    public ResponseEntity<FacultyDTO> createFaculty(@RequestBody FacultyDTO faculty) {
+        FacultyDTO createdFaculty = houseService.createFaculty(faculty);
         return ResponseEntity.ok(createdFaculty);
     }
 
     @GetMapping("/faculty/{facultyId}")
-    public ResponseEntity<Faculty> getFaculty(@PathVariable Long facultyId) {
-        Faculty faculty = houseService.findFaculty(facultyId);
+    public ResponseEntity<FacultyDTO> getFaculty(@PathVariable Long facultyId) {
+        FacultyDTO faculty = houseService.findFaculty(facultyId);
         if (faculty == null) {
             return ResponseEntity.notFound().build();
         }
@@ -32,14 +34,14 @@ public class HouseController {
     }
 
     @PutMapping("/faculty")
-    public ResponseEntity<Faculty> updateFaculty(@RequestBody Faculty faculty) {
-        Faculty updatedFaculty = houseService.updateFaculty(faculty);
+    public ResponseEntity<FacultyDTO> updateFaculty(@RequestBody FacultyDTO faculty) {
+        FacultyDTO updatedFaculty = houseService.updateFaculty(faculty);
         return ResponseEntity.ok(updatedFaculty);
     }
 
     @DeleteMapping("/faculty/{facultyId}")
-    public ResponseEntity<Faculty> deleteFaculty(@PathVariable Long facultyId) {
-        Faculty faculty = houseService.deleteFaculty(facultyId);
+    public ResponseEntity<FacultyDTO> deleteFaculty(@PathVariable Long facultyId) {
+        FacultyDTO faculty = houseService.deleteFaculty(facultyId);
         if (faculty == null) {
             return ResponseEntity.notFound().build();
         }
@@ -47,7 +49,19 @@ public class HouseController {
     }
 
     @GetMapping("/faculties")
-    public ResponseEntity<Collection<Faculty>> getAllStudentsByAge(@RequestParam String color) {
-        return ResponseEntity.ok(houseService.findFacultiesByColor(color));
+    public ResponseEntity<Collection<FacultyDTO>> findAllFaculties(@RequestParam(required = false) String color,
+                                                                   @RequestParam(required = false) String name) {
+        if (color != null) {
+            return ResponseEntity.ok(houseService.findFacultiesByColor(color));
+        }
+        if (name != null) {
+            return ResponseEntity.ok(houseService.findFacultyByName(name));
+        }
+        return ResponseEntity.ok(houseService.findAllFaculties());
+    }
+
+    @GetMapping("/faculty/students")
+    public ResponseEntity<Collection<StudentDTO>> getStudentsByFacultyId(Long facultyId) {
+        return ResponseEntity.ok(houseService.getStudentsByFacultyId(facultyId));
     }
 }
---
+++ src/main/java/ru/hogwarts/school/service/MappingUtils.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.dto.FacultyDTO;
+import ru.hogwarts.school.dto.StudentDTO;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+
+@Service
+public class MappingUtils {
+    public FacultyDTO mapFromFacultyToDTO(Faculty faculty) {
+        FacultyDTO dto = new FacultyDTO();
+        dto.setId(faculty.getId());
+        dto.setName(faculty.getName());
+        dto.setColor(faculty.getColor());
+        return dto;
+    }
+
+    public Faculty mapFromDTOtoFaculty(FacultyDTO dto) {
+        Faculty faculty = new Faculty();
+        faculty.setId(dto.getId());
+        faculty.setName(dto.getName());
+        faculty.setColor(dto.getColor());
+        return faculty;
+    }
+
+    public StudentDTO mapFromStudentToDTO(Student student) {
+        StudentDTO dto = new StudentDTO();
+        dto.setId(student.getId());
+        dto.setName(student.getName());
+        dto.setAge(student.getAge());
+        dto.setFacultyId(student.getFaculty().getId());
+        return dto;
+    }
+
+    public Student mapFromDTOtoStudent(StudentDTO dto) {
+        Student student = new Student();
+        student.setId(dto.getId());
+        student.setName(dto.getName());
+        student.setAge(dto.getAge());
+        return student;
+    }
+}
